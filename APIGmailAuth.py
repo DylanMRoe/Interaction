fc691becfad60b166863696d045dd81223be03f6
DylanMRoe <125489431+DylanMRoe@users.noreply.github.com>
2023-06-18 14:34:44 +0000

    Updating code

--- APIGmailAuth.py
+++ APIGmailAuth.py
@@ -1,19 +1,9 @@
-import tkinter as tk
-from tkinter import filedialog
-
-import matplotlib
-matplotlib.use('Agg')
-
 from google_auth_oauthlib.flow import Flow
 
-root = tk.Tk()
-root.withdraw()
+secFile = "C:\\Users\\dylan\\Downloads\\client_secret_199415634341-1i3p36rgi5atq2vf2u645p1kd9ku39fl.apps.googleusercontent.com.json"
 
-file_path = filedialog.askopenfilename
-print(file_path)
+flowSession = Flow.from_client_secrets_file(secFile, ["email"])
 
-# flowSession = Flow.from_client_secrets_file("client_secret_199415634341-fqauvjaqj7c84b3rbnvnpsh8b9tt1m1e.apps.googleusercontent.com.json",["email"])
+(x,y) = flowSession.authorization_url()
 
-#(x,y) = flowSession.authorization_url()
-
-#print(x)
+print(x)
