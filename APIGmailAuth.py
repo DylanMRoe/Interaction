c73aed01f1c2b8162dd5ca1da4ddf1bf7b1d56ed
DylanMRoe <125489431+DylanMRoe@users.noreply.github.com>
2023-06-18 16:48:20 +0000

    Updating code

--- APIGmailAuth.py
+++ APIGmailAuth.py
@@ -2,13 +2,13 @@ from google_auth_oauthlib.flow import Flow
 
 SCFILE="client_secret_199415634341-1i3p36rgi5atq2vf2u645p1kd9ku39fl.apps.googleusercontent.com.json"
 
-flowSession = Flow.from_client_secrets_file(SCFILE, ["email"], redirect_uri = "https://dylanmroe-obscure-goldfish-w6pp65xpq7jc9wjq.github.dev/")
+flowSession = Flow.from_client_secrets_file(SCFILE, ["https://www.googleapis.com/auth/gmail.send"], redirect_uri = "https://dylanmroe-obscure-goldfish-w6pp65xpq7jc9wjq.github.dev/")
 
-auth_Url,y = flowSession.authorization_url(prompt = "consent")
+auth_Url,y = flowSession.authorization_url()
 
 print("Please go to this URL: {}".format(auth_Url))
+print(y)
 
-code = input("Please enter the authorization code: ")
-flowSession.fetch_token(code=code)
+#flowSession.fetch_token(authorization_response = y)
 
-session = flowSession.authorized_session()
+#session = flowSession.authorized_session()
