0240d2e5c929be03c1dec409a141658c421074af
DylanMRoe <125489431+DylanMRoe@users.noreply.github.com>
2023-06-18 13:39:41 +0000

    Trying to fix tkinter issues

--- APIGmailAuth.py
+++ APIGmailAuth.py
@@ -1,5 +1,19 @@
+import tkinter as tk
+from tkinter import filedialog
+
+import matplotlib
+matplotlib.use('Agg')
+
 from google_auth_oauthlib.flow import Flow
 
-flowSession = Flow.from_client_secrets_file("client_secret_199415634341-fqauvjaqj7c84b3rbnvnpsh8b9tt1m1e.apps.googleusercontent.com.json",["email"])
+root = tk.Tk()
+root.withdraw()
 
-(x,y) = flowSession.authorization_url()
+file_path = filedialog.askopenfilename
+print(file_path)
+
+# flowSession = Flow.from_client_secrets_file("client_secret_199415634341-fqauvjaqj7c84b3rbnvnpsh8b9tt1m1e.apps.googleusercontent.com.json",["email"])
+
+#(x,y) = flowSession.authorization_url()
+
+#print(x)
